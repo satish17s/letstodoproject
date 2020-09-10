93c6f838f52a06c53f01849e7209574074ed0796
satish <satishsharma.cse@gmail.com>
2020-09-10 17:05:55 +0530

    postgres db added

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u-mf^!!3*5p0ylblrw-n8)*5u1zv!^bedxso!rchvl%jprb17_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['letstodoproject.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'mytodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'letstododb',
+        'USER':'postgres',
+        'PASSWORD':'Letspostgres@17',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
+
+
 
 
 # Password validation
